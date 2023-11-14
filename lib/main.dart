7cfc0682cf3a463edf1435d48b1e31f1cd09cf0a
Adhish Shakya <shakyaadhish@gmail.com>
2023-11-14 17:33:54 +0545

    dynamic background based on time

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:weather_app/data_service.dart';
+import 'package:weather_app/date_time_model.dart';
+import 'package:weather_app/date_time_service.dart';
 import 'package:weather_app/model.dart';
 
 void main() {
@@ -31,20 +34,55 @@ class _MyAppState extends State<MyApp> {
 
   final _dataService = DataService();
 
+  final _dateAndTimeService = DateAndTimeService();
+
   WeatherResponse? _response;
+
+  DateAndTimeResponse? _dateAndTimeResponse;
+
+  String? fullTime;
+  String? hour;
+
+  String backgroundImageStatus = "";
 
   void _searchCity(String city) async {
     final response = await _dataService.getWeather(city);
+
     setState(() {
       _response = response;
     });
-    // print(response.city);
-    // print(response.tempInfo.temperature);
-    // print(response.weatherInfo.description);
+
+    final dateAndTimeResponse = await _dateAndTimeService.getDateAndTime(
+      response.coordinatesInfo.latitude,
+      response.coordinatesInfo.longitude,
+    );
+
+    setState(() {
+      _dateAndTimeResponse = dateAndTimeResponse;
+      fullTime = _dateAndTimeResponse?.time;
+      hour = fullTime?.substring(11, 13);
+      int hourInt = int.parse(hour!);
+      print(hourInt);
+      print(hourInt.runtimeType);
+      // print(_dateAndTimeResponse?.time);
+      // print(response.city);
+      // print(response.tempInfo.temperature);
+      // print(response.weatherInfo.description);
+      // print(response.coordinatesInfo.longitude);
+      if (hourInt >= 05 && hourInt <= 11) {
+        backgroundImageStatus = "assets/morning.jpg";
+      } else if (hourInt >= 12 && hourInt <= 17) {
+        backgroundImageStatus = "assets/noon.jpg";
+      } else {
+        backgroundImageStatus = "assets/night.jpg";
+      }
+      print(backgroundImageStatus);
+    });
   }
 
   void onSearchButtonPress() {
     _searchCity(_cityName.text);
+    DateAndTimeService();
     _cityName.clear();
     FocusScope.of(context).requestFocus(
       FocusNode(),
@@ -54,18 +92,23 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Container(
-          decoration: const BoxDecoration(
+          decoration: BoxDecoration(
             image: DecorationImage(
-              image: AssetImage("assets/night.jpg"),
+              image: AssetImage(backgroundImageStatus.toString()),
               fit: BoxFit.cover,
             ),
           ),
           child: Padding(
             padding: const EdgeInsets.only(
-              top: 50,
+              top: 44,
               left: 40,
               right: 20,
             ),
@@ -116,42 +159,44 @@ class _MyAppState extends State<MyApp> {
                     ),
                   ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0),
-                  child: Container(
-                    width: 200,
-                    height: 200,
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(50),
+                Column(children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: Text(
+                      "${_response?.city}",
+                      style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
                       ),
                     ),
-                    child: Column(children: [
-                      Text(
-                        "${_response?.city}",
-                        style: const TextStyle(
-                          fontSize: 22,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Text(
-                        "${_response?.tempInfo.temperature}°",
-                        style: const TextStyle(
-                          fontSize: 40,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Text(
-                        "${_response?.weatherInfo.description}",
-                        style: const TextStyle(
-                          fontSize: 18,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ]),
                   ),
-                )
+                  Text(
+                    "${_response?.tempInfo.temperature}°",
+                    style: const TextStyle(
+                      fontSize: 70,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    "${_response?.weatherInfo.description[0].toUpperCase()}${_response?.weatherInfo.description.substring(1)}",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                  SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 470),
+                      child: Text(
+                        "${_dateAndTimeResponse?.time}",
+                        style: const TextStyle(
+                          fontSize: 34,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ])
               ],
             ),
           ),
